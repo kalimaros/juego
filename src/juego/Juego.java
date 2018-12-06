d24cb229f257a08de3bb4083c3dcf9f033092ac0
rocri <rocri.e.mc2@gmail.com>
2018-12-05 21:25:50 -0600

    Se corrige error del planeta

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -7,19 +7,19 @@ package juego;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
-public class Juego extends JFrame implements KeyListener, Runnable {
+public class Juego extends JFrame implements KeyListener, Runnable, ActionListener{
 
     protected int frameWidth = 500, frameHeight = 700;  ///declarando variables
     private int x = 210, y = 50;
     private int xasteroide = 450, yasteroide = 630;
-    int dx = 10, dy = 10;
-    private int xa = 210, ya = 50;
     private Image background = new ImageIcon(getClass().getResource("/resources/nebula.gif")).getImage();
     private int posicionX = 210;
     private int posicionY = 630;
@@ -27,10 +27,13 @@ public class Juego extends JFrame implements KeyListener, Runnable {
     private Image jumping = new ImageIcon(getClass().getResource("/resources/jumping.png")).getImage();
     private Image asteroide = new ImageIcon(getClass().getResource("/resources/asteroide.png")).getImage();
     private boolean jump = false;
+    private boolean ascendiendo = true;
     private boolean descendiendo = false;
     private boolean derecha = false;
     private boolean izquierdad = true;
-    private int u = 80, t = 1, g = 10;
+    private Thread thread;
+    private boolean corriendo;
+    private JButton reiniciarBoton;
 
     public Juego() {
         setBounds(50, 50, frameWidth, frameHeight);
@@ -40,72 +43,48 @@ public class Juego extends JFrame implements KeyListener, Runnable {
         setLocationRelativeTo(null);
         setResizable(false);
         setTitle("Jumping Planets");
-        iniciaHiloAsteroide();
-        iniciaHiloPlaneta();
     }
 
-    public void iniciaHiloAsteroide() {
-        Thread asteroideThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    if(izquierdad){
-                        xasteroide -= 30;
-                        if(xasteroide <= 0){
-                            izquierdad = false;
-                            derecha = true;
-                        }
-                    }
-                    if(derecha){
-                        xasteroide += 30;
-                        if(xasteroide >= 450){
-                            derecha = false;
-                            izquierdad = true;
-                        }
-                    }
-                    if(((xasteroide + 35) >= posicionX) && ((xasteroide - 35) <= posicionX) && posicionY > 600){
-                        break;
-                    }
-                    try {
-                        Thread.sleep(100); //original 80
-                    } catch (InterruptedException e) {
-                    }
-                    repaint();
-                }
+    public void renderizaAsteroide() {
+        if (izquierdad) {
+            xasteroide -= 30;
+            if (xasteroide <= 0) {
+                izquierdad = false;
+                derecha = true;
             }
-        });
-        asteroideThread.start();
+        }
+        if (derecha) {
+            xasteroide += 30;
+            if (xasteroide >= 450) {
+                derecha = false;
+                izquierdad = true;
+            }
+        }
+
     }
 
-    public void iniciaHiloPlaneta() {
-        Thread planetaThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    if (jump) {
-                        posicionY -= 50;
-                        if (posicionY <= 400) {
-                            jump = false;
-                            descendiendo = true;
-                        }
-                    }
+    public void renderizaNebula() {
+        if (jump) {
 
-                    if (descendiendo) {
-                        posicionY += 50;
-                        if (posicionY >= 600) {
-                            descendiendo = false;
-                        }
-
-                    }
-                    try {
-
-                        Thread.sleep(90); //original 80
-                    } catch (InterruptedException e) {
-                    }
+            if (ascendiendo) {
+                posicionY -= 50;
+                if (posicionY <= 400) {
+                    ascendiendo = false;
+                    descendiendo = true;
                 }
             }
-        });
-        planetaThread.start();
+
+            if (descendiendo) {
+                posicionY += 50;
+                if (posicionY >= 600) {
+                    descendiendo = false;
+                    jump = false;
+                    ascendiendo = true;
+                }
+
+            }
+        }
+
     }
 
     /**
@@ -113,14 +92,7 @@ public class Juego extends JFrame implements KeyListener, Runnable {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new Juego();
-        /*Juego nebula = new Nebula();
-        Juego planeta = new Planeta();*/
-
-        //Thread planetaThread = new Thread(planeta);
-
-        /*nebulaThread.start();
-        planetaThread.start();*/
+        new Juego().start();
     }
 
     @Override
@@ -142,7 +114,22 @@ public class Juego extends JFrame implements KeyListener, Runnable {
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        while (corriendo) {
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+            }
+            renderizaNebula();
+            renderizaAsteroide();
+            repaint();
+
+            if (((xasteroide + 35) >= posicionX) && ((xasteroide - 35) <= posicionX) && posicionY > 600) {
+                corriendo = false;
+                stop();
+            }
+
+        }
+
     }
 
     public void paint(Graphics g) {
@@ -151,6 +138,33 @@ public class Juego extends JFrame implements KeyListener, Runnable {
         g.drawImage(sitting, posicionX, posicionY, null);
         g.drawImage(asteroide, xasteroide, yasteroide, null);
 
+    }
+
+    public void start() {
+        thread = new Thread(this);
+        corriendo = true;
+        thread.start();
+    }
+
+    private void stop() {
+        corriendo = false;
+        getGraphics().clearRect(0, 0, frameWidth, frameHeight);
+        reiniciarBoton = new JButton("SALIR");
+        reiniciarBoton.setBounds(100, 150, 100, 30);
+        reiniciarBoton.addActionListener(this);
+        add(reiniciarBoton);
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==reiniciarBoton) {
+            System.out.println("");
+        }
     }
 
 }
