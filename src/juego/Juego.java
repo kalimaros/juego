4a0dd8c035df3d22f09a860a29ce58f1b79a19c8
rocri <rocri.e.mc2@gmail.com>
2018-12-06 07:34:22 -0600

    Se implementa boton y reinicio de juego

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -14,18 +14,21 @@ import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class Juego extends JFrame implements KeyListener, Runnable, ActionListener{
+public class Juego extends JFrame implements KeyListener, Runnable {
 
     protected int frameWidth = 500, frameHeight = 700;  ///declarando variables
     private int x = 210, y = 50;
-    private int xasteroide = 450, yasteroide = 630;
-    private Image background = new ImageIcon(getClass().getResource("/resources/nebula.gif")).getImage();
+    private int xasteroide = 450, yasteroide = 520;
+    private Image background = new ImageIcon(getClass().getResource("/resources/nebula.gif")).getImage().getScaledInstance(500, 600, 0);
     private int posicionX = 210;
-    private int posicionY = 630;
+    private int posicionY = 520;
     private Image sitting = new ImageIcon(getClass().getResource("/resources/sitting.png")).getImage();
     private Image jumping = new ImageIcon(getClass().getResource("/resources/jumping.png")).getImage();
     private Image asteroide = new ImageIcon(getClass().getResource("/resources/asteroide.png")).getImage();
+
     private boolean jump = false;
     private boolean ascendiendo = true;
     private boolean descendiendo = false;
@@ -34,21 +37,46 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
     private Thread thread;
     private boolean corriendo;
     private JButton reiniciarBoton;
+    private JPanel contentPane;
+    private boolean vivo;
 
     public Juego() {
-        setBounds(50, 50, frameWidth, frameHeight);
+        setBounds(100, 100, 0, 0);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         addKeyListener(this);
         setLocationRelativeTo(null);
-        setResizable(false);
         setTitle("Jumping Planets");
+
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        reiniciarBoton = new JButton("Jugar");
+        reiniciarBoton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                xasteroide = 450;
+                yasteroide = 520;
+                reiniciarBoton.setVisible(false);
+                requestFocus();
+                start();
+
+            }
+
+        });
+        reiniciarBoton.setBounds(210, 600, 89, 23);
+        reiniciarBoton.setVisible(false);
+        contentPane.add(reiniciarBoton);
+
+        setBounds(500, 0, frameWidth, frameHeight);
     }
 
     public void renderizaAsteroide() {
         if (izquierdad) {
             xasteroide -= 30;
-            if (xasteroide <= 0) {
+            if (xasteroide < 0) {
                 izquierdad = false;
                 derecha = true;
             }
@@ -68,7 +96,7 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
 
             if (ascendiendo) {
                 posicionY -= 50;
-                if (posicionY <= 400) {
+                if (posicionY <= 300) {
                     ascendiendo = false;
                     descendiendo = true;
                 }
@@ -76,7 +104,7 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
 
             if (descendiendo) {
                 posicionY += 50;
-                if (posicionY >= 600) {
+                if (posicionY >= 500) {
                     descendiendo = false;
                     jump = false;
                     ascendiendo = true;
@@ -103,7 +131,6 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
     @Override
     public void keyPressed(KeyEvent ke) {
         if (ke.getKeyCode() == KeyEvent.VK_SPACE) {
-
             jump = true;
         }
     }
@@ -123,7 +150,7 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
             renderizaAsteroide();
             repaint();
 
-            if (((xasteroide + 35) >= posicionX) && ((xasteroide - 35) <= posicionX) && posicionY > 600) {
+            if (((xasteroide + 35) >= posicionX) && ((xasteroide - 35) <= posicionX) && posicionY > 500) {
                 corriendo = false;
                 stop();
             }
@@ -132,8 +159,9 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
 
     }
 
-    public void paint(Graphics g) {
-        g.fillRect(0, 0, frameWidth, frameHeight);
+public void paint(Graphics g) {
+        super.paint(g);
+        g.fillRect(500, 0, frameWidth, frameHeight);
         g.drawImage(background, 0, 0, null);
         g.drawImage(sitting, posicionX, posicionY, null);
         g.drawImage(asteroide, xasteroide, yasteroide, null);
@@ -148,22 +176,11 @@ public class Juego extends JFrame implements KeyListener, Runnable, ActionListen
 
     private void stop() {
         corriendo = false;
-        getGraphics().clearRect(0, 0, frameWidth, frameHeight);
-        reiniciarBoton = new JButton("SALIR");
-        reiniciarBoton.setBounds(100, 150, 100, 30);
-        reiniciarBoton.addActionListener(this);
-        add(reiniciarBoton);
+        reiniciarBoton.setVisible(true);
         try {
             thread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==reiniciarBoton) {
-            System.out.println("");
         }
     }
 
